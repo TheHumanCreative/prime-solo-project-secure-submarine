5a1e9309eaa50128921b6e4b5608cb3398893f5f
Miles Lacek <mileslacek@gmail.com>
2019-09-04 15:12:57 -0500

    Authentication is operational and hidden secrects are viewable by the right user with access level.

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -3,13 +3,19 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
-        .then(results => res.send(results.rows))
-        .catch(error => {
-            console.log('Error making SELECT for secrets:', error);
+    if (req.isAuthenticated()) {
+        if (req.user.clearance_level >= 13) {
+        console.log("req.user:", req.user);
+        pool
+          .query('SELECT * FROM "secret";')
+          .then(results => res.send(results.rows))
+          .catch(error => {
+            console.log("Error making SELECT for secrets:", error);
             res.sendStatus(500);
-        });
+          });
+    } else {
+        res.sendStatus(403);
+    }}
 });
 
 module.exports = router;
