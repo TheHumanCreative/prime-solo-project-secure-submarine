f39995a31ac25bd36c5c7b87f960249d33f8a773
Miles Lacek <mileslacek@gmail.com>
2019-09-04 15:35:54 -0500

    All users able to see their respective accessible secret.

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     if (req.isAuthenticated()) {
-        if (req.user.clearance_level >= 13) {
         console.log("req.user:", req.user);
+        queryText = 'SELECT * FROM "secret" WHERE "secrecy_level" <= $1 ;';
         pool
-          .query('SELECT * FROM "secret";')
-          .then(results => res.send(results.rows))
+          .query(queryText, [req.user.clearance_level]) // moved the query text to be in a variable
+          .then(results => res.send(results.rows)) // sources the user.clearance_level user's clearance level allows the to see the content.
           .catch(error => {
             console.log("Error making SELECT for secrets:", error);
             res.sendStatus(500);
@@ -16,6 +16,6 @@ router.get('/', (req, res) => {
     } else {
         res.sendStatus(403);
     }}
-});
+);
 
 module.exports = router;
